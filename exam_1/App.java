b69d5860478e462ded7949407b76de263fe0d5a1
markcaram47 <111546363+markcaram47@users.noreply.github.com>
2022-09-06 09:47:47 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] arg) {
+
+        //Student Class
+        Student Me = new Student();
+
+        Me.surname = "Caram";
+        Me.firstname = "Jan Mark";
+        Me.middleInitial = 'S';
+        Me.dateOfbirth = 12/22/1003;
+        Me.studentNumber = 2022102151;
+        Me.studentEmailAddress = "caramjs@students.national-u.edu.ph";
+        Me.iAmAwesome = true;
+        Me.sayMyStudentNumber();
+        Me.sayMyEmailAddress();
+        Me.AmIAwesome();
+
+
+        }
+    }
+
