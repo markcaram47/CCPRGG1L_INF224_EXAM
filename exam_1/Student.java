e79a83a75c3c680ebdb423b27d5ee7a7d83a4b41
markcaram47 <111546363+markcaram47@users.noreply.github.com>
2022-09-06 09:49:21 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    //Student Properties
+    String surname;
+    String firstname;
+    char middleInitial;
+    int dateOfbirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+
+    //Student Class Method
+    public void sayMyStudentNumber() {
+        System.out.println("My Student Number is " + studentNumber);
+    }    
+    public void sayMyEmailAddress() {
+        System.out.println("My Email Address is " + studentEmailAddress);
+    }
+    public void AmIAwesome() {
+        System.out.println("Am I Awesome " + iAmAwesome);
+    }
+}
